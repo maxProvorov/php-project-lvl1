1ca9f63ccdd7e206c69260ce633a850cd6f3fae3
Max <maxiys1@rambler.ru>
2024-07-17 21:55:27 +0300

    Исправлена ошибка тестов

--- src/Cli.php
+++ src/Cli.php
@@ -8,7 +8,8 @@ use function cli\prompt;
 function greetUser()
 {
     line('Welcome to the Brain Games!');
-    $name = prompt('May I have your name?');
+    echo 'May I have your name? ';
+    $name = rtrim(fgets(STDIN), ":\n");
     line("Hello, %s!", $name);
     return $name;
 }
